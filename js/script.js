8aa8460fa1bbc9f3649f4f4aa70bb4eb25c2f328
Ravi Silva <ravifelipee@gmail.com>
2024-12-06 07:29:20 -0300

    Getting Started with Implementing Programming Logic in JavaScript

---
+++ js/script.js
@@ -0,0 +1,77 @@
+// Variable declaration
+const question = document.querySelector("#question"); // Questions
+const answersBox = document.querySelector("#answers-box"); // Alternatives
+const quizzContainer = document.querySelector("#quizz-container"); // Quizz
+const scoreContainer = document.querySelector("#score-container"); // Score board
+let letters = ["a", "b", "c", "d"];
+let points = 0;
+let actualQuestion = 0; // To find out which question the quizz is on
+
+// Questions
+const questions = [
+    {
+        "question": "PHP foi desenvolvido para qual fim?",
+        "answers": [
+            {
+                "answer": "back-end",
+                "correct": true
+            },
+            {
+                "answer": "front-end",
+                "correct": false
+            },
+            {
+                "answer": "Sistema operacional",
+                "correct": false
+            },
+            {
+                "answer": "Banco de dados",
+                "correct": false
+            },
+        ]
+    },
+    {
+        "question": "Uma forma de declarar variável em JavaScript:",
+        "answers": [
+            {
+                "answer": "$var",
+                "correct": false
+            },
+            {
+                "answer": "var",
+                "correct": true
+            },
+            {
+                "answer": "@var",
+                "correct": false
+            },
+            {
+                "answer": "#let",
+                "correct": false
+            },
+        ]
+    },
+    {
+        "question": "Qual o seletor de id no CSS?",
+        "answers": [
+            {
+                "answer": "#",
+                "correct": true
+            },
+            {
+                "answer": ".",
+                "correct": false
+            },
+            {
+                "answer": "@",
+                "correct": false
+            },
+            {
+                "answer": "/",
+                "correct": false
+            },
+        ]
+    },
+]
+
+// Quiz replacement for first question
